3eab47ed71edd21e2f17faa5280e3d2c8360436c
Chen Xiaojie <caliburn1994@gmail.com>
2022-03-15 21:39:03 +0900

    refactor: improve code

--- app/src/main/kotlin/kyakya/icu/javalin/demo/web/HeathCheckController.kt
+++ app/src/main/kotlin/kyakya/icu/javalin/demo/web/HeathCheckController.kt
@@ -7,8 +7,7 @@ class HeathCheckController {
 
     private val gitInfo by lazy {
         val config = this::class.java.getResource("/git-info.yml")?.readText()
-        return@lazy ObjectMapper(YAMLFactory())
-            .readValue(config, Map::class.java)
+        ObjectMapper(YAMLFactory()).readValue(config, Map::class.java)
     }
 
     fun get(): Any {
